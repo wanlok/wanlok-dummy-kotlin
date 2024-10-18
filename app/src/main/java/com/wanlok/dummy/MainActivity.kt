ca1d29259e4e98906891b142fe5cd042092a3139
Robert Wan <wantunglok@gmail.com>
2024-10-19 10:22:19 +1100

    add share function

--- app/src/main/java/com/wanlok/dummy/MainActivity.kt
+++ app/src/main/java/com/wanlok/dummy/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.wanlok.dummy
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,14 +13,21 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var shareButton: Button
 
+    fun share(text: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        startActivity(Intent.createChooser(intent, null))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val text = "Hello World"
         shareButton = findViewById(R.id.share_button)
-
-        shareButton.setOnClickListener { Log.d(TAG, "CHECKING") }
+        shareButton.setOnClickListener { share(text) }
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
