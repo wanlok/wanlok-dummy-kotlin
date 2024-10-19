650815cc79abc6c8446b972f125ebe7105f94e23
Robert Wan <wantunglok@gmail.com>
2024-10-20 10:19:05 +1100

    refactor code

--- app/src/main/java/com/wanlok/dummy/MainActivity.kt
+++ app/src/main/java/com/wanlok/dummy/MainActivity.kt
@@ -2,15 +2,15 @@ package com.wanlok.dummy
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
-
-    val TAG: String = "MainActivity"
-
+    val tag: String = "MainActivity"
     lateinit var shareButton: Button
 
     fun share(text: String) {
@@ -22,17 +22,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val text = "Hello World"
         shareButton = findViewById(R.id.share_button)
         shareButton.setOnClickListener { share(text) }
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
     }
 }
